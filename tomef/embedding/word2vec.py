57b629a3ee932486c55afcf62ef9d8224488ae65
Markus Plack <markus.plack@gmail.com>
2019-05-14 12:12:47 +0200

    Add faster word2vec token filter
    The previous filter used get_close_matches to find the best fitting
    token. Since this was rather slow, the new filter only uses the first
    version of a phrase that it finds. All subsequent versions are dropped
    from the lookup dictionaries.
    
    The old filter can still be used by setting the fast_filter option in
    the embedding config to false.

--- tomef/embedding/word2vec.py
+++ tomef/embedding/word2vec.py
@@ -71,12 +71,50 @@ class Word2VecFilter():
             idx += max(valid,1)
         return tokens
 
+class FastWord2VecFilter(Word2VecFilter):
+    def build_lookup(self, vocab):
+        self.lookup = {}
+        for token in ProgressIterator(vocab, print_every=10000):
+            words = token.split('_')
+            if '' in words or len(words) == 0:
+                continue
+            current_lookup = self.lookup
+            for word in words:
+                lower_word = word.lower()
+                if not lower_word in current_lookup:
+                    current_lookup[lower_word] = {}
+                current_lookup = current_lookup[lower_word]
+            if exists_token not in current_lookup:
+                current_lookup[exists_token] = token
+    
+    def filter(self, all_tokens):
+        idx = 0
+        tokens = []
+        while idx < len(all_tokens):
+            phrase = None
+            currentidx = idx
+            valid = 0
+            current_lookup = self.lookup
+            while currentidx < len(all_tokens) and all_tokens[currentidx].lower() in current_lookup:
+                current_lookup = current_lookup[all_tokens[currentidx].lower()]
+                if exists_token in current_lookup:
+                    phrase = current_lookup[exists_token]
+                    valid = valid + 1
+                currentidx = currentidx + 1
+            if phrase is not None:
+                tokens.append(phrase)
+            elif self.track_exclusion:
+                self.excluded[all_tokens[idx]] += 1
+            idx += max(valid,1)
+        return tokens
+    
 
 class Word2vecModel(WordembeddingModel):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self._filter = None
         self.track_exclusion = self.info.get('track_exclusion', False)
+        self.fast_filter = self.info.get('fast_filter', True)
     
     def _load_model(self):
         if not data.embedding_file_exists(self.info):
@@ -104,6 +142,9 @@ class Word2vecModel(WordembeddingModel):
     def filter(self):
         if self._filter is None:
             nbprint('Loading word2vec filter...')
-            self._filter = Word2VecFilter(self.vocab, self.track_exclusion)
+            if self.fast_filter:
+                self._filter = FastWord2VecFilter(self.vocab, self.track_exclusion)
+            else:
+                self._filter = Word2VecFilter(self.vocab, self.track_exclusion)
             nbprint.clear_last()
         return self._filter
